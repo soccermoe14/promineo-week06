ec4269c21c82b59cf8625f02c34de896d9340561
soccermoe14 <114617209+soccermoe14@users.noreply.github.com>
2022-11-08 13:38:36 -0600

    first commit

---
+++ warGame.js
@@ -0,0 +1,25 @@
+/*
+Coding Steps:
+•	For the final project you will be creating an automated version of the classic card game WAR! There are many versions of the game WAR. In this version there are only 2 players.
+o	You do not need to do anything special when there is a tie in a round.
+•	Think about how you would build this project and write your plan down. Consider classes such as: Card, Deck, Player, as well as what properties and methods they may include. 
+o	You do not need to accept any user input, when you run your code, the entire game should play out instantly without any user input inside of your browser’s console.
+The completed project should, when executed, do the following:
+•	Deal 26 Cards to each Player from a Deck of 52 cards.
+•	Iterate through the turns where each Player plays a Card.
+•	The Player who played the higher card is awarded a point
+o	Ties result in zero points for both Players
+•	After all cards have been played, display the score and declare the winner.
+•	Write a Unit Test using Mocha and Chai for at least one of the functions you write.
+*/
+
+/* Need to make a deck of 52 cards
+    -Do I need to have suits?
+    -How to give cards values?
+How to create the runs that run automatically?
+Create 2 players
+Must play ALL cards to determine winner
+Need to display the score for each player after all cards played
+Need to declare player with highest score winner
+DONT FORGET to run a chai mocha test on a function
+*/
